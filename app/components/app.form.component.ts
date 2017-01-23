0875a4bf7d1dd7b32aa0e26d972f5a73af847f80
David Chow <yip_keung_chow@hotmail.com>
2017-01-23 11:30:38 +1100

    test git on Visual Studio

--- app/components/app.form.component.ts
+++ app/components/app.form.component.ts
@@ -1,6 +1,12 @@
 import { Component ,OnInit} from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+ function emailChecker(input: FormControl){
+        //let regex='/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+        let result=new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(input.value);
+        return result?null:{emailError:true};
+}
+
 @Component({
     selector: 'my-app',
     templateUrl: 'app/templates/app.form.template.html',
@@ -11,6 +17,8 @@ export class AppFormComponent implements OnInit {
     username: FormControl;
     email: FormControl;
 
+   
+
     constructor(private builder: FormBuilder) {}
 
     ngOnInit() {
@@ -20,8 +28,10 @@ export class AppFormComponent implements OnInit {
         ]);
         this.email = new FormControl('', [
             Validators.required,
-            Validators.minLength(3)
+            Validators.minLength(3),
+            emailChecker
         ]);
+        //formGroup must be setup after all formControl created. it is pass by values.
          this.myForm = this.builder.group({
             username: this.username,
             email: this.email
